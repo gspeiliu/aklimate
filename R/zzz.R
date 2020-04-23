78f30a73e1c51575afbcd5f02ff05f98eb52b163
Vlado Uzunangelov <uzunangelov@gmail.com>
2020-04-22 20:59:03 -0700

    added some comments to DESCRIPTION and zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
 .onLoad <- function(libname, pkgname) {
+  ##create a composite binary operator in global environment
   `%docomb%` <<- if(foreach::getDoParRegistered()) foreach::`%dopar%` else foreach::`%do%`
 }
